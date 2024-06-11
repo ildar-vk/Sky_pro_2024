5fc1f591207ed7481405e62b2e7f07bc80af492c
Ildar.K.V <kalimov.ildar.v@gmail.com>
2024-06-12 04:52:57 +0500

    first_DZ_for_Sky_prep

---
+++ src/masks.py
@@ -0,0 +1,18 @@
+def get_mask_card_number(card_number):
+    visible_digits = 6
+    masked_number = (
+        card_number[:visible_digits]
+        + " "
+        + " ".join(
+            ["**" if i < visible_digits + 2 else "****" for i in range(visible_digits, len(card_number) - 4, 4)]
+        )
+        + " "
+        + card_number[-4:]
+    )
+    return masked_number
+
+
+def get_mask_account(account_number):
+    visible_digits = 4
+    masked_number = "**" + account_number[-visible_digits:]
+    return masked_number

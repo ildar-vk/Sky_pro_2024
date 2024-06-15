f3d31350df9e2e618bec6b26f72afa3f6b453526
Ildar.K.V <kalimov.ildar.v@gmail.com>
2024-06-16 02:35:50 +0500

    Сначало прогнал flake8 и mypy (коммит не создался, видимо из за того что в коде не было изменений), далее прогнала black код реструктурировался и удалось создать коммит. Остался isort.

--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,18 @@
-def filter_by_state(data, state='EXECUTED'):
-    '''
+def filter_by_state(data, state="EXECUTED"):
+    """
 
     :param data: в функцию в виде аргумента подается список словарей
     :param state: по умолчанию 'EXECUTED' возвращает новый список, содержащий только те словари, у которых ключ
                 state
                 содержит переданное в функцию значение.
-    '''
-    return [d for d in data if d.get('state') == state]
+    """
+    return [d for d in data if d.get("state") == state]
+
 
 def sort_by_date(data, reverse=True):
-    '''
+    """
 
     :param data: в функцию в виде аргумента подается список словарей
     :param reverse: второй необязательный задает порядок сортировки (убывание, возрастание).
-    '''
-    return sorted(data, key=lambda x: x['date'], reverse=reverse)
+    """
+    return sorted(data, key=lambda x: x["date"], reverse=reverse)

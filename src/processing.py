646bc888c394f3ad5e4ef4109df8820bd84b98ad
Ildar.K.V <kalimov.ildar.v@gmail.com>
2024-06-16 02:19:17 +0500

    Предварительный, не форматированный код.

---
+++ src/processing.py
@@ -0,0 +1,17 @@
+def filter_by_state(data, state='EXECUTED'):
+    '''
+
+    :param data: в функцию в виде аргумента подается список словарей
+    :param state: по умолчанию 'EXECUTED' возвращает новый список, содержащий только те словари, у которых ключ
+                state
+                содержит переданное в функцию значение.
+    '''
+    return [d for d in data if d.get('state') == state]
+
+def sort_by_date(data, reverse=True):
+    '''
+
+    :param data: в функцию в виде аргумента подается список словарей
+    :param reverse: второй необязательный задает порядок сортировки (убывание, возрастание).
+    '''
+    return sorted(data, key=lambda x: x['date'], reverse=reverse)

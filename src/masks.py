f46c97353077b2cc114e939e1a4ac13fbaf58302
Ildar.K.V <kalimov.ildar.v@gmail.com>
2024-06-20 08:22:59 +0500

    Правильный срез карты пытыюсь мёрджить

--- src/masks.py
+++ src/masks.py
@@ -1,15 +1,7 @@
 def get_mask_card_number(card_number):
-    visible_digits = 6
-    masked_number = (
-        card_number[:visible_digits]
-        + " "
-        + " ".join(
-            ["**" if i < visible_digits + 2 else "****" for i in range(visible_digits, len(card_number) - 4, 4)]
-        )
-        + " "
-        + card_number[-4:]
-    )
+    masked_number = card_number[:4] + ' '+card_number[4:6]+'**' + ' **** ' + card_number[-4:]
     return masked_number
+
 
 
 def get_mask_account(account_number):

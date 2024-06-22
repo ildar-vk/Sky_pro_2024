e26b5194fbf282d45e3c22dedc5a7cba76e8871a
Ildar.K.V <kalimov.ildar.v@gmail.com>
2024-06-22 11:35:23 +0500

    22.06 Кривые коммиты :)       Работа над тестами processing.py       Создал тесты на основе ДЗ 10_1 -> Ok, но в первой функции проеряются при разных значении второго параметра функции, не реализовал.

--- src/processing.py
+++ src/processing.py
@@ -2,9 +2,10 @@ def filter_by_state(data:list[dict], state="EXECUTED")->list[dict]:
     """
 
     :param data: в функцию в виде аргумента подается список словарей
-    :param state: по умолчанию 'EXECUTED' возвращает новый список, содержащий только те словари, у которых ключ
-                state
-                содержит переданное в функцию значение.
+    :param state: по умолчанию 'EXECUTED' возвращает новый список, содержащий только те словари,
+                у которых ключ state содержит переданное в функцию значение.
+
+
     """
     return [d for d in data if d.get("state") == state]
 
---
+++ tests/test_processing.py
@@ -0,0 +1,10 @@
+import pytest
+
+import src.processing
+
+
+def test_filter_by_state_sorted_list(Sorted_list):
+    assert src.processing.filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
+
+def test_sort_by_date_sorted_date(Sorted_date):
+    assert src.processing.sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])

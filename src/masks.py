ec0796ff7f9e2f878a770f0ddb4c98f32f91c971
Ildar.K.V <kalimov.ildar.v@gmail.com>
2024-06-22 09:48:35 +0500

    22.06 Исправил фунции 9.1       Работая над тестами masks.py       подгружаю тесты в проект, ранее удаленные как я понимаю в локальном репозитории в Гит я их вижу.

--- src/masks.py
+++ src/masks.py
@@ -1,22 +1,13 @@
 
 
-def get_mask_card_number(card_number:str)->str:
-    visible_digits = 6
-    card_number = (
-        card_number[:visible_digits]
-        + " "
-        + " ".join(
-            ["**" if i < visible_digits + 2 else "****" for i in range(visible_digits, len(card_number) - 4, 4)]
-        )
-        + " "
-        + card_number[-4:]
-    )
-    return card_number
+def get_mask_card_number(card_number):
+    masked_number = card_number[:4] + ' '+card_number[4:6]+'**' + ' **** ' + card_number[-4:]
+    return masked_number
 
-
-def get_mask_account(account_number:str)->str:
+def get_mask_account(account_number):
     visible_digits = 4
-    account_number = "**" + account_number[-visible_digits:]
-    return account_number
+    masked_number = '**' + account_number[-visible_digits:]
+    return masked_number
+# Исправленная 22.06
 
 
--- tests/tests_main.py
+++
@@ -1,5 +0,0 @@
-def tests_one():
-    1==1
-
-def new_changer():
-    pass

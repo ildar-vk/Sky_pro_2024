cb0cf597644ac304d29ff9dbf077cfca1284419e
Ildar.K.V <kalimov.ildar.v@gmail.com>
2024-06-22 11:34:04 +0500

    22.06 Исправил фунции 9.1       Работа над тестами processing.py       Создал тесты на основе ДЗ -> Ok, но в первой функции проеряются при разных значении второго параметра функции, не реализовал.

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -9,12 +9,6 @@ def test_get_mask_card_number_Ghost_card(Ghost_card):
 def test_get_mask_account_Ghost_card_account(Ghost_card_account):
     assert src.masks.get_mask_account('7000792289606361') == Ghost_card_account
 
-# def test_mask_number_length():
-#     assert src.masks.get_mask_card_number("12345678901234567890") == "123456 ****** 7890"
-#     assert src.masks.get_mask_card_number("98765") == "98765"
-#
-# def test_mask_number_invalid_input():
-#     assert src.masks.get_mask_card_number("") is None
-#     assert src.masks.get_mask_card_number("12345a6789012345") is None
+
 
 

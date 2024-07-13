2c855cb6a33eb5da8209dbb5f5987f9518382306
ildar-vk <kalimov.ildar.v@gmail.com>
2024-07-14 00:15:22 +0500

    13.07 ДЗ 12_1

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,47 +1,25 @@
 import json
-import unittest
 from unittest.mock import patch
-
-from src.external_api import convert_currency
-from src.external_api import process_transaction
-
+from src.utils import input_json
 
 def test_input_json():
-    filename = "C:\\Users\Professional\PycharmProjects\Sky_pro_2024_1\data\operation.json"
-    try:
-        with open(filename, "r", encoding="utf-8") as file:
-            content = file.read()
-            if "[" not in content and "]" not in content:
-                print("Файл не содержит списков")
-            else:
-                with open(filename, "s", encoding="utf-8") as f:
-                    data = json.load(f)
-                    print(data)
-    except FileNotFoundError:
-        print("Файл не найден")
+    with patch('builtins.open') as mock_open:
+        mock_file = mock_open.return_value.__enter__.return_value
+        mock_file.read.return_value = json.dumps({
+    "id": 185048835,
+    "state": "EXECUTED",
+    "date": "2019-05-06T00:17:42.736209",
+    "operationAmount": {
+      "amount": "74895.83",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод со счета на счет",
+    "from": "Счет 27921306202254867520",
+    "to": "Счет 49884962711830774470"
+  })
+        assert input_json("../data/operations.json") ==[]
 
 
-class TestConvertCurrency(unittest.TestCase):
-
-    @patch("src.external_api.requests.get")
-    def test_convert_currency(self, mock_get):
-        mock_response = mock_get.return_value
-        mock_response.json.return_value = {"result": 75.0}  # Пример вымышленного результата
-        converted_amount = convert_currency(50, "USD")
-        self.assertEqual(converted_amount, 75.0)
-
-
-
-
-
-class TestProcessTransaction(unittest.TestCase):
-
-    @patch("src.utils.convert_currency")
-    def test_process_transaction(self, mock_convert_currency):
-        file_json = {"amount": 100, "currency": "USD"}
-
-        mock_convert_currency.return_value = 75
-
-        result = process_transaction(file_json)
-
-        self.assertEqual(result, 75)

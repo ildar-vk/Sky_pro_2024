1de72bda150e316da44c536dcf4403a7fb4463cd
ildar-vk <kalimov.ildar.v@gmail.com>
2024-07-22 02:35:49 +0500

    22.07 ДЗ 12_2

--- src/utils.py
+++ src/utils.py
@@ -1,33 +1,43 @@
-
+import csv
 import json
+
+import pandas as pd
+
 from src.decorators import logger_utils
+
+
 @logger_utils
-def input_json(file_json):
+def input_data(file_path):
     """
-    Это определение функции input_json,
-    которая принимает аргумент: file_json (файл с данными о финансовых транзакциях)
-    и возвращает список словарей с данными о финансовых транзакциях.
-    Если файл пустой, содержит не список или не найден,
-    функция возвращает пустой список.
+    Функция input_data принимает аргумент file_path (путь к файлу с данными о финансовых транзакциях) и
+    возвращает список словарей с данными о финансовых транзакциях.
+    Если файл пустой, содержит не список или не найден, функция возвращает пустой список.
+    Поддерживаемые форматы: JSON, CSV, XLSX.
     """
     try:
-        with open(file_json, "r", encoding="utf-8") as file:
-            content = file.read()
-            if "[" not in content and "]" not in content:
-                logger_utils.error("Файл не содержит списков")
-                empty_list = []
-                return empty_list
-            else:
-                with open(file_json, encoding="utf-8") as f:
-                    data = json.load(f)
-                    logger_utils.info("Файл со списками")
+        if file_path.endswith(".json"):
+            with open(file_path, "r", encoding="utf-8") as file:
+                data = json.load(file)
+                logger_utils.info("Прочитан файл JSON")
                 return data
+        elif file_path.endswith(".csv"):
+            data = []
+            with open(file_path, newline="", encoding="utf-8") as file:
+                csv_reader = csv.DictReader(file)
+                for row in csv_reader:
+                    data.append(row)
+            logger_utils.info("Прочитан файл CSV")
+            return data
+        elif file_path.endswith(".xlsx"):
+            data = pd.read_excel(file_path).to_dict(orient="records")
+            logger_utils.info("Прочитан файл XLSX")
+            return data
+        else:
+            logger_utils.error("Неподдерживаемый формат файла")
+            return []
     except FileNotFoundError:
         logger_utils.error("Файл не найден")
-        empty_list = []
-        return empty_list
-    except json.JSONDecodeError:
-        logger_utils.error("Ошибка декодирования JSON")
         return []
-
-
+    except Exception as e:
+        logger_utils.error(f"Произошла ошибка: {e}")
+        raise
